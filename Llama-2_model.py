dc72feaf278547a10c9b9e1da99723364ea31864
Nam Tran Minh <81914645+trminhnam@users.noreply.github.com>
2023-09-06 09:19:54 +0700

    Update Llama-2_model.py

--- Llama-2_model.py
+++ Llama-2_model.py
@@ -2,12 +2,24 @@ import transformers
 import torch
 import json
 
-model = "meta-llama/Llama-2-7b-chat-hf"
+from transformers import AutoModelForCausalLM, BitsAndBytesConfig, AutoTokenizer
+
+
+bnb_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_compute_dtype=torch.bfloat16,
+    bnb_4bit_use_double_quant=True,
+)
+model_name_or_path = "meta-llama/Llama-2-7b-chat-hf"
+model = AutoModelForCausalLM.from_pretrained(model_name_or_path, quantization_config=bnb_config, cache_dir="./cache")
+tokenizer = AutoTokenizer.from_pretrained(model_name_or_path, cache_dir="./cache")
 
 # Khởi tạo mô hình Llama-2
 pipeline = transformers.pipeline(
     "text-generation",
     model=model,
+    tokenizer=tokenizer,
     torch_dtype=torch.float16,
     device_map="auto",
 )
